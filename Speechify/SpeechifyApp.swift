ddf0e0b4077f6707d7f3ab13b1faa933e70b8e12
Olorunshola Godwin <godwin@bleeptv.co>
2021-01-23 10:43:11 +0100

    Initial Commit

---
+++ Speechify/SpeechifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechifyApp.swift
+//  Speechify
+//
+//  Created by Olorunshola Godwin on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpeechifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
